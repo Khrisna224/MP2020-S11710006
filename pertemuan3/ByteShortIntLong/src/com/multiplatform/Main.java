ffe05a63f68dbeeb92d8d9795fdcf24123bc37e0
Khrisna224 <32382686+Khrisna224@users.noreply.github.com>
2020-06-08 16:01:54 +0800

    update pertemuan 3

--- pertemuan3/ByteShortIntLong/src/com/multiplatform/Main.java
+++ pertemuan3/ByteShortIntLong/src/com/multiplatform/Main.java
@@ -3,6 +3,17 @@ package com.multiplatform;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myValue = 10000;
+
+	    int myMaxIntValue = Integer.MAX_VALUE;
+	    int myMinIntValue = Integer.MIN_VALUE;
+        System.out.println("Integer Maximum value = " + myMaxIntValue);
+        System.out.println("Integer Minimum value = " + myMinIntValue);
+        System.out.println("Overflow = " + (myMaxIntValue + 1));
+        System.out.println("Underflow = " + (myMinIntValue - 1));
+
+        int maxValue = 2_147_483_647; // hanya untuk memudahkan membaca
+
+        //byte, short, long
     }
 }
