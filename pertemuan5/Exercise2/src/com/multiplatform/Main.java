d3e64b5440790ae85fed52661019b9592f7174b5
Khrisna224 <32382686+Khrisna224@users.noreply.github.com>
2020-06-11 15:51:21 +0800

    edit exercise 5 & update pertemuan 6

--- pertemuan5/Exercise2/src/com/multiplatform/Main.java
+++ pertemuan5/Exercise2/src/com/multiplatform/Main.java
@@ -1,5 +1,8 @@
 package com.multiplatform;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,5 +11,14 @@ public class Main {
 	    double kg = 0.45359237d;
         System.out.println(pound + " Pound = " +(pound*kg) + " kg");
         System.out.println("Dibulatkan = "+String.format("%,.2f",pound*kg) + " kg");
+
+
+        //Experiment
+        Scanner inputInteger = new Scanner(System.in);
+        System.out.print("\n\nInput Pound = ");
+        int pound2 = inputInteger.nextInt();
+        System.out.println(pound + " Pound = " +(pound2*kg) + " kg");
+        System.out.println("Integer = " + ((int)(pound2*kg)) + " kg");
+        System.out.println("Integer = " + String.format("%,.0f",pound2*kg) + " kg");
     }
 }
