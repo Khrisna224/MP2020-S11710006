d594205d7c3321ad838930a00024771786fc710d
Khrisna224 <32382686+Khrisna224@users.noreply.github.com>
2020-06-12 15:35:50 +0800

    Update pertemuan 7

---
+++ pertemuan7/Method/src/com/multiplatform/Main.java
@@ -0,0 +1,19 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    printMessage("John");
+	    int res = addition(0,0);
+    }
+    public static void printMessage(String name){
+        System.out.println("Hello " + name);
+    }
+    public static  int addition(int a, int b){
+        if(a==0 && b==0){
+            return -1;
+        }
+        int result = a + b;
+        return result;
+    }
+}
