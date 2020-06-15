cf1070691be78ae9412917b55a621093a1a1232a
Khrisna224 <32382686+Khrisna224@users.noreply.github.com>
2020-06-15 15:32:28 +0800

    update pertemuan 8

---
+++ pertemuan8/MethodOverloading/src/com/multiplatform/Main.java
@@ -0,0 +1,14 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Method Overloading
+    }
+    public static int sum(int a, int b){
+        return a + b;
+    }
+    public static int sum(int a, double b){
+        return a + (int) b;
+    }
+}
