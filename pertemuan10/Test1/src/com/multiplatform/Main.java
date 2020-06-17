edc2aec2a5b8975ec6b3a6b8b62d8d31f844acb2
Khrisna224 <32382686+Khrisna224@users.noreply.github.com>
2020-06-17 15:58:59 +0800

    update pertemuan 10(done no 2)

--- pertemuan10/Test1/src/com/multiplatform/Main.java
+++ pertemuan10/Test1/src/com/multiplatform/Main.java
@@ -4,15 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
         // No 1
+        System.out.println("No 1");
         System.out.println(shouldWakeUp (true, 1));
         System.out.println(shouldWakeUp (false, 2));
         System.out.println(shouldWakeUp (true, 8));
         System.out.println(shouldWakeUp (true, -1));
 
+        // No 2
+        System.out.println("\nNo 2");
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
     }
+    
     public static boolean shouldWakeUp(boolean banking, int hourOfDay){
         if ((hourOfDay < 0 || hourOfDay > 23) || banking == false ) return false;
         else if (hourOfDay < 8 || hourOfDay > 22) return true;
+        else return false;
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (summer == true){
+            if (temperature > 25 && temperature <= 45) return true;
+            else return false;
+        }
+        else if (summer == false){
+            if (temperature > 25 && temperature <= 35) return true;
+            else return false;
+        }
         return false;
     }
 }
